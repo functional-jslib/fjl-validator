b6c6b7be2e0bcf3d03b7eb1a00caf487cb556602
Ely De La Cruz <elycruz@elycruz.com>
2018-10-26 17:57:17 -0400

    deps_update - './tests/test-validator-NotEmptyValidator' tests updated to use jest.

--- tests/test-validator-NotEmptyValidator.js
+++ tests/test-validator-NotEmptyValidator.js
@@ -2,21 +2,20 @@
  * Created by elyde on 1/15/2016.
  */
 import {notEmptyValidator} from '../src/notEmptyValidator';
-import {expect, assert} from 'chai';
 
 describe('notEmptyValidator', function () {
 
     test ('should return `true` if value is not `empty`.', function () {
         let validator = notEmptyValidator({});
         ['hello', 99, true, [1], {a: 1}].forEach(function (value) {
-            expect(validator(value).result).to.equal(true);
+            expect(validator(value).result).toEqual(true);
         });
     });
 
     test ('should return `false` if value is `empty`.', function () {
         let validator = notEmptyValidator({});
         ['', 0, false, [], {}].forEach(function (value) {
-            expect(validator(value).result).to.equal(false);
+            expect(validator(value).result).toEqual(false);
         });
     });
 
@@ -24,8 +23,8 @@ describe('notEmptyValidator', function () {
         let validator = notEmptyValidator({});
         ['', 0, false, [], {}].forEach(function (value) {
             const {result, messages} = validator(value);
-            expect(result).to.equal(false);
-            expect(messages.length > 0).to.equal(true);
+            expect(result).toEqual(false);
+            expect(messages.length > 0).toEqual(true);
         });
     });
 
